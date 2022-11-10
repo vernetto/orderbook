75bdad9a9db9bcbd81cfd5094fe7c3778572a179
Michał Kostro <michal.kostro@10g.pl>
2022-11-10 13:12:17 +0100

    added springdoc-openapi-ui and configured swagger for OrderController

--- src/main/java/com/example/orderbook/controllers/OrderController.java
+++ src/main/java/com/example/orderbook/controllers/OrderController.java
@@ -3,6 +3,11 @@ package com.example.orderbook.controllers;
 import com.example.orderbook.entities.OrderEntry;
 import com.example.orderbook.exceptions.OrderBookException;
 import com.example.orderbook.services.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +24,11 @@ public class OrderController {
     }
 
     @PostMapping("/createOrder")
+    @Operation(description = "Creates now order",
+        requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(schema = @Schema(implementation = OrderEntry.class), mediaType = MediaType.APPLICATION_JSON_VALUE), required = true),
+        responses = {
+            @ApiResponse(responseCode = "200",content = @Content(schema = @Schema(implementation = OrderEntry.class), mediaType = MediaType.APPLICATION_JSON_VALUE))
+        })
     public OrderEntry createOrder(@RequestBody OrderEntry orderEntry) throws OrderBookException {
         return orderService.createOrder(orderEntry);
     }
