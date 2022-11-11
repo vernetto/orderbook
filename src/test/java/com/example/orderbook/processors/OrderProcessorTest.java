2b140916f5ca202f496e3c170b020181a5ec354f
vernetto <Ernesto0z>
2022-11-11 09:55:12 +0100

    order processor unittest3

--- src/test/java/com/example/orderbook/processors/OrderProcessorTest.java
+++ src/test/java/com/example/orderbook/processors/OrderProcessorTest.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OrderProcessorTest {
 
@@ -24,17 +25,31 @@ class OrderProcessorTest {
     @Test
     void processExecution() {
         OrderProcessor orderProcessor = new OrderProcessor();
+        // 2 orders, different price, execution will match the second and partially fill it
         OrderEntry orderEntry1 = new OrderEntry(1L , "ISIN1", BigDecimal.valueOf(100), BigDecimal.valueOf(100), TIME_0, OrderType.BUY, BigDecimal.valueOf(5), OrderEntryStatus.OPEN);
         OrderEntry orderEntry2 = new OrderEntry(2L , "ISIN1", BigDecimal.valueOf(100), BigDecimal.valueOf(100), TIME_1, OrderType.BUY, BigDecimal.valueOf(6), OrderEntryStatus.OPEN);
         List<OrderEntry> orders = Arrays.asList(orderEntry1, orderEntry2);
-        Execution execution =  new Execution(1L, "ISIN1", BigDecimal.valueOf(60), BigDecimal.valueOf(5.5), ExecutionType.OFFER);
+        Execution execution1 =  new Execution(1L, "ISIN1", BigDecimal.valueOf(60), BigDecimal.valueOf(5.5), ExecutionType.OFFER);
         List<OrderEntry> affectedOrders = new ArrayList<>();
-        orderProcessor.processExecution(orders, execution, affectedOrders);
-        assertNotNull(affectedOrders);
+        orderProcessor.processExecution(orders, execution1, affectedOrders);
         assertEquals(1, affectedOrders.size());
-        OrderEntry orderEntry = affectedOrders.get(0);
-        assertEquals(2L, orderEntry.getId());
-        assertEquals(BigDecimal.valueOf(40), orderEntry.getAvailableQuantity());
+        OrderEntry affectedOrderEntry1 = affectedOrders.get(0);
+        assertEquals(2L, affectedOrderEntry1.getId());
+        assertEquals(BigDecimal.valueOf(40), affectedOrderEntry1.getAvailableQuantity());
+
+        // another execution will fill both orders
+        Execution execution2 =  new Execution(2L, "ISIN1", BigDecimal.valueOf(200), BigDecimal.valueOf(5), ExecutionType.OFFER);
+        List<OrderEntry> affectedOrders2 = new ArrayList<>();
+        orderProcessor.processExecution(orders, execution2, affectedOrders2);
+        assertEquals(2, affectedOrders2.size());
+        OrderEntry affectedOrderEntry2_1 = affectedOrders2.get(0);
+        OrderEntry affectedOrderEntry2_2 = affectedOrders2.get(1);
+        assertEquals(1L, affectedOrderEntry2_1.getId());
+        assertEquals(2L, affectedOrderEntry2_2.getId());
+        assertEquals(BigDecimal.ZERO, affectedOrderEntry2_1.getAvailableQuantity());
+        assertEquals(BigDecimal.ZERO, affectedOrderEntry2_2.getAvailableQuantity());
+        assertEquals(OrderEntryStatus.FILLED, affectedOrderEntry2_1.getStatus());
+        assertEquals(OrderEntryStatus.FILLED, affectedOrderEntry2_2.getStatus());
 
     }
 }
