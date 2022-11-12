ba960482feb9a534703f4afe62933e14866e50f9
özkan pakdil <ozkan.pakdil@gmail.com>
2022-11-12 14:25:32 +0000

    maybe related to #7 (#9)

--- src/main/java/com/example/orderbook/exceptions/OrderBookException.java
+++ src/main/java/com/example/orderbook/exceptions/OrderBookException.java
@@ -1,5 +1,9 @@
 package com.example.orderbook.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class OrderBookException extends Exception {
     public ExceptionCode getExceptionCode() {
         return exceptionCode;
